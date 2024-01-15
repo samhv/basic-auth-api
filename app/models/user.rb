7146ea33077fb961e0038a835ab63a41631d8935
Sam Hernandez <samhernandez@Sams-MacBook-Pro.local>
2024-01-14 17:21:00 -0800

    add validations to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+    VALID_PASSWORD_REGEX = /\A
+        (?=.*[A-Za-z]) # At least one letter
+        (?=.*\d)       # At least one number
+        (?=.*[!@#$%^&*()_+{}\[\]:;<>,.?]) # At least one special character
+    /x
+
+    has_secure_password
+    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :password, presence: true, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }
 end
